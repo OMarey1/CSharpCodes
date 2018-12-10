ea6546a34167b60fb0090e5803f25a155a7c0dac
Omar Marey <omarey191@outlook.com>
2018-12-10 21:22:24 +0200

    FirstProgramme
    This is my FirstProgramme in C#

---
+++ FirstProgramme/FirstProgramme/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgramme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is my first programme in C#
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
