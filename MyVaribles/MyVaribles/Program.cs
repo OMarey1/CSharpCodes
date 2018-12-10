d5ded5ff66167ed0fa7c5b8ee5451ecf8f0a9c3a
Omar Marey <omarey191@outlook.com>
2018-12-10 21:31:40 +0200

    FirstVariables
    This is my FirstVariables in C#

---
+++ MyVaribles/MyVaribles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVaribles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            Console.Write("{", num1, "}");
+
+        }
+    }
+}
